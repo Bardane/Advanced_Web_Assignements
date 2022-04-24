522585d5dabd66f0bd4535bd6c29f039449b32c6
Théo <theo.martin@efrei.net>
2022-04-24 12:03:48 -0700

    Add Assignment 1 Folder

---
+++ Assignement1/index.js
@@ -0,0 +1,11 @@
+var nom = "Théo"
+var project = "Advanced Web Project r/place remake"
+
+printTest(nom, project)
+
+function printTest(nom, project){
+	console.log(project)
+	console.log("by " + nom)
+}
+
+
